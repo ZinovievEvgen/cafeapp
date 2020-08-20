7f40f6a2f62a8985fec2bb103096d93fde34a7f1
ZinovievEvgen <natsy29@mail.ru>
2020-08-21 00:17:02 +0400

    Modified User.java (hashcode...)

--- src/main/java/back/models/User.java
+++ src/main/java/back/models/User.java
@@ -47,4 +47,110 @@ public class User {
 
     public User() {
     }
+
+    public Long getIdOfUser() {
+        return idOfUser;
+    }
+
+    public void setIdOfUser(Long idOfUser) {
+        this.idOfUser = idOfUser;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public List<CoffeeOrder> getCoffeeOrdersOfUser() {
+        return coffeeOrdersOfUser;
+    }
+
+    public void setCoffeeOrdersOfUser(List<CoffeeOrder> coffeeOrdersOfUser) {
+        this.coffeeOrdersOfUser = coffeeOrdersOfUser;
+    }
+
+    public String getEmailOfUser() {
+        return emailOfUser;
+    }
+
+    public void setEmailOfUser(String emailOfUser) {
+        this.emailOfUser = emailOfUser;
+    }
+
+    public String getPassOfUser() {
+        return passOfUser;
+    }
+
+    public void setPassOfUser(String passOfUser) {
+        this.passOfUser = passOfUser;
+    }
+
+    public String getLoginOfUser() {
+        return loginOfUser;
+    }
+
+    public void setLoginOfUser(String loginOfUser) {
+        this.loginOfUser = loginOfUser;
+    }
+
+    public String getNameOfUser() {
+        return nameOfUser;
+    }
+
+    public void setNameOfUser(String nameOfUser) {
+        this.nameOfUser = nameOfUser;
+    }
+
+    public String getSurnameOfUser() {
+        return surnameOfUser;
+    }
+
+    public void setSurnameOfUser(String surnameOfUser) {
+        this.surnameOfUser = surnameOfUser;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (idOfUser != null ? !idOfUser.equals(user.idOfUser) : user.idOfUser != null) return false;
+        if (surnameOfUser != null ? !surnameOfUser.equals(user.surnameOfUser) : user.surnameOfUser != null)
+            return false;
+        if (nameOfUser != null ? !nameOfUser.equals(user.nameOfUser) : user.nameOfUser != null) return false;
+        if (loginOfUser != null ? !loginOfUser.equals(user.loginOfUser) : user.loginOfUser != null) return false;
+        if (passOfUser != null ? !passOfUser.equals(user.passOfUser) : user.passOfUser != null) return false;
+        if (emailOfUser != null ? !emailOfUser.equals(user.emailOfUser) : user.emailOfUser != null) return false;
+        if (coffeeOrdersOfUser != null ? !coffeeOrdersOfUser.equals(user.coffeeOrdersOfUser) : user.coffeeOrdersOfUser != null)
+            return false;
+        return roles != null ? roles.equals(user.roles) : user.roles == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idOfUser != null ? idOfUser.hashCode() : 0;
+        result = 31 * result + (surnameOfUser != null ? surnameOfUser.hashCode() : 0);
+        result = 31 * result + (nameOfUser != null ? nameOfUser.hashCode() : 0);
+        result = 31 * result + (loginOfUser != null ? loginOfUser.hashCode() : 0);
+        result = 31 * result + (passOfUser != null ? passOfUser.hashCode() : 0);
+        result = 31 * result + (emailOfUser != null ? emailOfUser.hashCode() : 0);
+        result = 31 * result + (coffeeOrdersOfUser != null ? coffeeOrdersOfUser.hashCode() : 0);
+        result = 31 * result + (roles != null ? roles.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "loginOfUser='" + loginOfUser + '\'' +
+                ", passOfUser='" + passOfUser + '\'' +
+                ", emailOfUser='" + emailOfUser + '\'' +
+                '}';
+    }
 }
