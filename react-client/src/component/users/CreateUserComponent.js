4c495bf6009fb599776859e3d8b1e3e00e838813
ZinovievEvgen <natsy29@mail.ru>
2020-10-04 14:55:04 +0400

    Correct CreateUserComponent, UpdateUserComponent, check this page

--- react-client/src/component/users/CreateUserComponent.js
+++ react-client/src/component/users/CreateUserComponent.js
@@ -1,8 +1,191 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import UserAxiosService from '../../service/UserAxiosService';
 
 class CreateUserComponent extends Component {
-    
+
+    constructor(props) {
+        super(props);
+        this.onChangeSurnameOfUser = this.onChangeSurnameOfUser.bind(this);
+        this.onChangeNameOfUser = this.onChangeNameOfUser.bind(this);
+        this.onChangeLoginOfUser = this.onChangeLoginOfUser.bind(this);
+        this.onChangePassOfUser = this.onChangePassOfUser.bind(this);
+        this.onChangeEmailOfUser = this.onChangeEmailOfUser.bind(this);
+        this.saveUser = this.saveUser.bind(this);
+        this.newUser = this.newUser.bind(this);
+
+        this.state = {
+            idOfUser: null,
+            surnameOfUser: "",
+            nameOfUser: "",
+            loginOfUser: "",
+            passOfUser: "",
+            emailOfUser: "",
+
+
+            submitted: false
+        };
+    }
+
+    onChangeSurnameOfUser(e) {
+        this.setState({
+            surnameOfUser: e.target.value
+        });
+    }
+
+    onChangeNameOfUser(e) {
+        this.setState({
+            nameOfUser: e.target.value
+        });
+    }
+
+    onChangeLoginOfUser(e) {
+        this.setState({
+            loginOfUser: e.target.value
+        });
+    }
+
+    onChangeEmailOfUser(e) {
+        this.setState({
+            emailOfUser: e.target.value
+        });
+    }
+
+    onChangePassOfUser(e) {
+        this.setState({
+            passOfUser: e.target.value
+        });
+    }
+
+    saveUser() {
+        var data = {
+            surnameOfUser: this.state.surnameOfUser,
+            nameOfUser: this.state.nameOfUser,
+            loginOfUser: this.state.loginOfUser,
+            passOfUser: this.state.passOfUser,
+            emailOfUser: this.state.emailOfUser
+        };
+
+        UserAxiosService.createUser(data)
+            .then(response => {
+                this.setState({
+                    idOfUser: response.data.idOfUser,
+                    surnameOfUser: response.data.surnameOfUser,
+                    nameOfUser: response.data.nameOfUser,
+                    loginOfUser: response.data.loginOfUser,
+                    passOfUser: response.data.passOfUser,
+                    emailOfUser: response.data.emailOfUser,
+
+                    submitted: true
+                });
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
+    newUser() {
+        this.setState({
+            idOfUser: null,
+            surnameOfUser: "",
+            nameOfUser: "",
+            loginOfUser: "",
+            passOfUser: "",
+            emailOfUser: "",
+
+            submitted: false
+        });
+    }
+
+    cancel() {
+        this.props.history.push('/users');
+    }
+
+    render() {
+        return (
+            <div className="submit-form">
+                {this.state.submitted ? (
+                    <div>
+                        <h4>Информация сохранена!</h4>
+                        <button className="btn btn-success" onClick={this.newUser}>
+                            Добавить
+                        </button>
+                    </div>
+                ) : (
+                    <div>
+                        <div className="form-group">
+                            <label htmlFor="title">Фамилия:</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="surnameOfUser"
+                                required
+                                value={this.state.surnameOfUser}
+                                onChange={this.onChangeSurnameOfUser}
+                                name="surnameOfUser"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="description">Имя:</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="nameOfUser"
+                                required
+                                value={this.state.nameOfUser}
+                                onChange={this.onChangeNameOfUser}
+                                name="nameOfUser"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="description">Логин:</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="loginOfUser"
+                                required
+                                value={this.state.loginOfUser}
+                                onChange={this.onChangeLoginOfUser}
+                                name="loginOfUser"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="description">Пароль:</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="passOfUser"
+                                required
+                                value={this.state.passOfUser}
+                                onChange={this.onChangePassOfUser}
+                                name="passOfUser"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="description">Электронная почта:</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="emailOfUser"
+                                required
+                                value={this.state.emailOfUser}
+                                onChange={this.onChangeEmailOfUser}
+                                name="emailOfUser"
+                            />
+                        </div>
+
+                        <button onClick={this.saveUser} className="btn btn-success">
+                            Сохранить
+                        </button>
+                        <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>
+                            Отмена </button>
+                    </div>
+                )}
+            </div>
+        )
+    }
+
 }
 
 export default  CreateUserComponent
--- react-client/src/component/users/UpdateUserComponent.js
+++ react-client/src/component/users/UpdateUserComponent.js
@@ -1,8 +1,200 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import UserAxiosService from '../../service/UserAxiosService';
 
 class UpdateUserComponent extends Component {
 
+    constructor(props) {
+        super(props);
+        this.onChangeSurnameOfUser = this.onChangeSurnameOfUser.bind(this);
+        this.onChangeNameOfUser = this.onChangeNameOfUser.bind(this);
+        this.onChangeLoginOfUser = this.onChangeLoginOfUser.bind(this);
+        this.onChangePassOfUser = this.onChangePassOfUser.bind(this);
+        this.onChangeEmailOfUser = this.onChangeEmailOfUser.bind(this);
+        this.getUser = this.getUser.bind(this);
+        this.updateUser = this.updateUser.bind(this);
+        this.deleteUser = this.deleteUser.bind(this);
+
+        this.state = {
+            currentCoffeeType: {
+                idOfUser: null,
+                surnameOfUser: "",
+                nameOfUser: "",
+                loginOfUser: "",
+                passOfUser: "",
+                emailOfUser: ""
+            },
+            message: ""
+        };
+    }
+
+    componentDidMount() {
+        this.getUser(this.props.match.params.id);
+    }
+
+    onChangeSurnameOfUser(e) {
+        const surnameOfUser = e.target.value;
+
+        this.setState(function (prevState) {
+            return {
+                currentUser: {
+                    ...prevState.currentUser,
+                    surnameOfUser: surnameOfUser
+                }
+            };
+        });
+    }
+
+    onChangeNameOfUser(e) {
+        const nameOfUser = e.target.value;
+
+        this.setState(prevState => ({
+            currentUser: {
+                ...prevState.currentUser,
+                nameOfUser: nameOfUser
+            }
+        }));
+    }
+
+    onChangeLoginOfUser(e) {
+        const loginOfUser = e.target.value;
+
+        this.setState(prevState => ({
+            currentUser: {
+                ...prevState.currentUser,
+                loginOfUser: loginOfUser
+            }
+        }));
+    }
+
+    onChangePassOfUser(e) {
+        const passOfUser = e.target.value;
+
+        this.setState(prevState => ({
+            currentUser: {
+                ...prevState.currentUser,
+                passOfUser: passOfUser
+            }
+        }));
+    }
+
+    onChangeEmailOfUser(e) {
+        const emailOfUser = e.target.value;
+
+        this.setState(prevState => ({
+            currentUser: {
+                ...prevState.currentUser,
+                emailOfUser: emailOfUser
+            }
+        }));
+    }
+
+    getUser(idOfUser) {
+        UserAxiosService.getUserById(idOfUser)
+            .then(response => {
+                this.setState({
+                    currentUser: response.data
+                });
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
+    updateUser() {
+        UserAxiosService.updateUser(
+            this.state.currentUser.idOfUser,
+            this.state.currentUser
+            )
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    message: "Обновление прошло успешно!"
+                });
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
+    deleteUser() {
+        UserAxiosService.deleteUser(this.state.currentUser.idOfUser)
+            .then(response => {
+                console.log(response.data);
+                this.props.history.push('/users')
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
+    cancel() {
+        this.props.history.push('/users');
+    }
+
+    render() {
+        const {currentUser} = this.state;
+
+        return (
+            <div>
+                {currentUser ? (
+                    <div className="edit-form">
+                        <h4>Выбранный пользователь</h4>
+                        <form>
+                            <div className="form-group">
+                                <label>Фамилия:</label>
+                                <input type="text" className="form-control" id="surnameOfUser"
+                                       value={currentUser.surnameOfUser} onChange={this.onChangeSurnameOfUser}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Имя:</label>
+                                <input type="text" className="form-control" id="nameOfUser"
+                                       value={currentUser.nameOfUser} onChange={this.onChangeNameOfUser}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Логин:</label>
+                                <input type="text" className="form-control" id="loginOfUser"
+                                       value={currentUser.loginOfUser} onChange={this.onChangeLoginOfUser}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Пароль:</label>
+                                <input type="text" className="form-control" id="passOfUser"
+                                       value={currentUser.passOfUser} onChange={this.onChangePassOfUser}/>
+                            </div>
+                            <div className="form-group">
+                                <label>Электронная почта:</label>
+                                <input type="text" className="form-control" id="emailOfUser"
+                                       value={currentUser.emailOfUser} onChange={this.onChangeEmailOfUser}/>
+                            </div>
+                        </form>
+
+                        <button
+                            type="submit"
+                            className="btn btn-sm btn-success"
+                            onClick={this.updateUser}>
+                            Обновить
+                        </button>
+
+                        <button
+                            className="btn btn-sm btn-danger"
+                            onClick={this.deleteUser} style={{marginLeft: "10px"}}>
+                            Удалить
+                        </button>
+
+                        <button className="btn btn-sm btn-danger"
+                                onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}> Отмена
+                        </button>
+                        <p>{this.state.message}</p>
+                    </div>
+                ) : (
+                    <div>
+                        <br />
+                        <p>Выберите нужного пользователя...</p>
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
 
 export default  UpdateUserComponent
